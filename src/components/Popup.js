9f985a8deb02bb06d94cdd0e3a7c52939de5fe16
Dasha Kholobaeva <dashaholobaeva@gmail.com>
2023-04-12 13:54:12 +0300

    fix: fixed EscEventlistener in Popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,22 +1,19 @@
 export default class Popup {
     constructor(popupSelector) {
         this._popupElement = document.querySelector(popupSelector);
+        this._handleEscClose = this._handleEscClose.bind(this)
     }
 
     open() {
         this._popupElement.classList.add('popup_opened');
 
-        document.addEventListener('keydown', (event) => {
-            this._handleEscClose(event);
-        });
+        document.addEventListener('keydown', this._handleEscClose);
     }
 
     close() {
         this._popupElement.classList.remove('popup_opened');
 
-        document.removeEventListener('keydown', (event) => {
-            this._handleEscClose(event);
-        });
+        document.removeEventListener('keydown', this._handleEscClose);
     }
 
     _handleEscClose(event) {
